2814693d50b8e9d26878eebb58baa9b26cd6b408
Isaac Melo <isaacalvesx7@gmail.com>
2021-12-17 18:17:52 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-
 import Dificulty from "./Components/Dificulty";
-
 function App() {
   return (
     <div className="App">
---
+++ src/Components/Dificulty/styles.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Component = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  height: 100vh;
+  width: 100%;
+  margin: auto;
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+export const DificultyLevel = styled.div`
+  height: 40%;
+  width: 90%;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px #335577;
+  background-color: #102132;
+  margin: auto;
+  text-align: center;
+  padding-top: 10px;
+
+  cursor: pointer;
+  @media (max-width: 1000px) {
+    height: 90%;
+    margin-bottom: 10px;
+  }
+
+  @media (min-width: 1400px) {
+    height: 65%;
+  }
+  :hover {
+    filter: brightness(110%);
+  }
+  h1 {
+    color: #fff;
+  }
+
+  img {
+    width: 70%;
+    height: 70%;
+    border-radius: 10px;
+  }
+`;
+
+export const H1 = styled.h1`
+  color: white;
+  text-align: center;
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
